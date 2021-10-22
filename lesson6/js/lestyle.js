0f05262fdb27a4b2eb4c5cb1c1b5026f2ff98e5f
PugaKevin <pug20001@byui.edu>
2021-10-22 00:28:39 -0600

    WindChill

--- lesson6/js/lestyle.js
+++ lesson6/js/lestyle.js
@@ -25,12 +25,4 @@ if(day === 5) {
 }else {
     infobanner.style.display = "none";
 }
-/*Calculate Wind chill*/
-let temp = parseFloat(document.querySelector('#temp').textContent);
-let speed = parseFloat(document.querySelector('#speed').textContent);
-// Math time
-let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-//round down to make sure we dont' get a long number
-let windchill = Math.floor(wc);
-//Output
-document.querySelector('#windchilla').innerHTML = `${windchill}`; 
+
---
+++ lesson6/js/windchill.js
@@ -0,0 +1,16 @@
+/*Calculate Wind chill*/
+let temp = parseFloat(document.querySelector('#temp').textContent);
+let speed = parseFloat(document.querySelector('#speed').textContent);
+// Math time
+let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+//round down to make sure we dont' get a long number
+let windchill = Math.floor(wc);
+//Output
+let istemp = isNaN(temp);
+let isspeed = isNaN(speed);
+
+if(istemp === "True" || isspeed === "True"){
+    document.querySelector('#windchilla').innerHTML = `N/A`;
+}else{
+    document.querySelector('#windchilla').innerHTML = `${windchill}`; 
+}
