231fa59796b6a6737a103aa2bf276fe413bf039e
PugaKevin <pug20001@byui.edu>
2021-10-22 00:39:22 -0600

    windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -6,11 +6,15 @@ let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp *
 //round down to make sure we dont' get a long number
 let windchill = Math.floor(wc);
 //Output
-let istemp = isNaN(temp);
-let isspeed = isNaN(speed);
 
-if(istemp === "True" || isspeed === "True"){
+if(temp <= 50 && speed > 3){
+    let istemp = isNaN(temp);
+    let isspeed = isNaN(speed);
+    if(istemp === "True" || isspeed === "True"){
+        document.querySelector('#windchilla').innerHTML = `N/A`;
+    }else{
+        document.querySelector('#windchilla').innerHTML = `${windchill}`; 
+    }
+}else {
     document.querySelector('#windchilla').innerHTML = `N/A`;
-}else{
-    document.querySelector('#windchilla').innerHTML = `${windchill}`; 
 }
